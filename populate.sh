e809e3a49ca7ed5ea9e3c4079a62f3eb618dd9b7
Matthew Murphy <matthew.murphy2@phila.gov>
2016-01-11 13:01:46 -0500

    let's try wget

--- populate.sh
+++ populate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ssh citycom2@philadelphiavotes.com "cp pvotes.all.sql.gz public_html/"
-curl -s http://www.philadelphiavotes.com/pvotes.all.sql.gz
+ssh citycom2@philadelphiavotes.com "cp pvotes.all.sql.gz public_html/files/"
+curl -s http://www.philadelphiavotes.com/files/pvotes.all.sql.gz
 gunzip < pvotes.all.sql.gz | sed 's/www\.philadelphiavotes\.com/192.168.33.22/g' | sed  's/philadelphiavotes\.com/192.168.33.22/g' | sed 's/\/home\/citycom2/\/var\/www/g' | mysql -u${DBUSER} -p${DBPASS} ${DBNAME}
-ssh citycom2@philadelphiavotes.com "rm public_html/pvotes.all.sql.gz"
+ssh citycom2@philadelphiavotes.com "rm public_html/files/pvotes.all.sql.gz"
