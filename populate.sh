c32c0eb408aebd5bbdff75c2b5a237757d34fe13
Matthew Murphy <matthew.murphy2@phila.gov>
2016-01-11 13:14:23 -0500

    shell-only script to pull and load the siteDB (vagrant ssh -c 'sh /var/www/populate.sh')

--- populate.sh
+++ populate.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ssh citycom2@philadelphiavotes.com "cp pvotes.all.sql.gz public_html/files/"
-curl -s http://www.philadelphiavotes.com/files/pvotes.all.sql.gz
+wget http://www.philadelphiavotes.com/files/pvotes.all.sql.gz
 gunzip < pvotes.all.sql.gz | sed 's/www\.philadelphiavotes\.com/192.168.33.22/g' | sed  's/philadelphiavotes\.com/192.168.33.22/g' | sed 's/\/home\/citycom2/\/var\/www/g' | mysql -u${DBUSER} -p${DBPASS} ${DBNAME}
 ssh citycom2@philadelphiavotes.com "rm public_html/files/pvotes.all.sql.gz"
+
