ffa5a9368d451582996be5c227327a6d53761a1e
Matthew Murphy <matthew.murphy2@phila.gov>
2016-02-17 21:18:03 -0500

    ssh called

--- populate.sh
+++ populate.sh
@@ -22,4 +22,4 @@ date
 echo "done"
 
 echo "getting site (excluding public_html/files/*)"
-tar citycom2@philadelphiavotes.com "tar --exclude='public_html/files/*' czf - public_html" | "tar - xzf"
+ssh citycom2@philadelphiavotes.com "tar --exclude='public_html/files/*' czf - public_html" | "tar - xzf"
